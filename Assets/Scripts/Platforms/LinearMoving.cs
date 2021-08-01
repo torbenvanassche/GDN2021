094561a8c63d8def7a4ef8420c143e3a2be0119a
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-08-01 15:49:38 -0500

    chore: cleaned up code a bit, gave example prefabs;

--- Assets/Scripts/Platforms/LinearMoving.cs
+++ Assets/Scripts/Platforms/LinearMoving.cs
@@ -9,35 +9,33 @@ public class LinearMoving : MonoBehaviour
     public float travelDistance = 1;
     public float speed = 1f;
     private Vector3 startPosition;
-    Vector3 baseTransformVector;
+    Vector3 movementDirection;
 
     void Start()
     {
         startPosition = transform.position;
+        movementDirection = GetMovementDirection();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-        if (direction == Direction.X)
+    private Vector3 GetMovementDirection()
+    //  Get Vector3 to act as an axis for the object to move along.
+    {
+        switch (direction)
         {
-            baseTransformVector = new Vector3(1, 0, 0);
-        }
-        else if (direction == Direction.Y)
-        {
-            baseTransformVector = new Vector3(0, 1, 0);
-        }
-        else
-        {
-            baseTransformVector = new Vector3(0, 0, 0);
-            Debug.LogError($"No such direction for {gameObject.name}: {direction}");
+            case Direction.X:
+                return new Vector3(1, 0, 0);
+            case Direction.Y:
+                return new Vector3(0, 1, 0);
+            default:
+                Debug.LogError($"No such direction for {gameObject.name}: {direction}");
+                return new Vector3(0, 0, 0);
         }
     }
 
     private void FixedUpdate()
     {
-        Vector3 offset = travelDistance * Mathf.Cos((Mathf.PI / 2) * Time.time * speed) * baseTransformVector;
+        Vector3 offset = movementDirection * travelDistance * Mathf.Cos((Mathf.PI / 2) * Time.time * speed);
         transform.position = startPosition + offset;
     }
 }
