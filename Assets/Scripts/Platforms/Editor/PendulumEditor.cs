a6ee588840ee17ef4461cf16837bc49d5fb07b49
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-08-02 13:00:49 -0500

    chore: made the pendulum editor beziers slightly more accurate;

--- Assets/Scripts/Platforms/Editor/PendulumEditor.cs
+++ Assets/Scripts/Platforms/Editor/PendulumEditor.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class PendulumEditor : Editor
 {
     private Pendulum _pendulum;
-    
+
     private void Awake()
     {
         _pendulum = target as Pendulum;
@@ -19,8 +19,8 @@ public class PendulumEditor : Editor
         var xMin = new Vector3(_pendulum.transform.position.x - _pendulum.xRadius, _pendulum.transform.position.y, _pendulum.transform.position.z);
         var xMax = new Vector3(_pendulum.transform.position.x + _pendulum.xRadius, _pendulum.transform.position.y, _pendulum.transform.position.z);
         var yMin = new Vector3(_pendulum.transform.position.x, _pendulum.transform.position.y - _pendulum.yRadius, _pendulum.transform.position.z);
-        
-        Handles.DrawBezier(xMin, yMin, Vector3.zero, Vector3.zero, Color.red, null, 1);
-        Handles.DrawBezier(yMin, xMax, Vector3.zero, Vector3.zero, Color.red, null, 1);
+
+        Handles.DrawBezier(xMin, yMin, xMin + 0.25f * _pendulum.yRadius * Vector3.down, yMin + 0.25f * _pendulum.xRadius * Vector3.left, Color.red, null, 1);
+        Handles.DrawBezier(yMin, xMax, yMin + 0.25f * _pendulum.xRadius * Vector3.right, xMax + 0.25f * _pendulum.yRadius * Vector3.down, Color.green, null, 1);
     }
 }
