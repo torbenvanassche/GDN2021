ca3f137653b0fb22e267f8bc34477bd090b99e56
confusedphoenix <karaphoenixnicole@gmail.com>
2021-08-08 11:48:29 -0400

    I think that's everything

--- Assets/Scripts/CutsceneOne.cs
+++ Assets/Scripts/CutsceneOne.cs
@@ -6,25 +6,18 @@ public class CutsceneOne : MonoBehaviour
 {
 
     public Camera cinemaCam;
-    public GameObject cinemaPlayer;
+    public GameObject playerPet;
+    public GameObject pet;
+    public GameObject dialogue;
+    public GameObject moveStop;
 
     public Animation spiritAnim;
     public Animation petAnim;
-    public Animation spiritAnimTwo;
 
-    public Animation spiritAnimIdle;
-    public Animation petAnimIdle;
 
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(CutsceneTimer());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
 
@@ -32,6 +25,15 @@ public class CutsceneOne : MonoBehaviour
     {
         //yield return new WaitForSeconds(4);
         spiritAnim.Play("spiritDecent");
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(5);
+        dialogue.SetActive(true);
+        yield return new WaitForSeconds(10);
+        pet.SetActive(true);
+        petAnim.Play("pullApart");
+        yield return new WaitForSeconds(5);
+        playerPet.SetActive(true);
+        pet.SetActive(false);
+        moveStop.SetActive(false);
+        Controls.Instance.Input.Enable();
     }
 }
