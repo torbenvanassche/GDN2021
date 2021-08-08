03ada9e2c6fc66e5fd6062ac6d583024790c0b8a
confusedphoenix <karaphoenixnicole@gmail.com>
2021-08-08 00:30:59 -0400

    levels so far

---
+++ Assets/Scripts/CutsceneOne.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneOne : MonoBehaviour
+{
+
+    public Camera cinemaCam;
+    public GameObject cinemaPlayer;
+
+    public Animation spiritAnim;
+    public Animation petAnim;
+    public Animation spiritAnimTwo;
+
+    public Animation spiritAnimIdle;
+    public Animation petAnimIdle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator CutsceneTimer()
+    {
+
+    }
+}
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MenuController : MonoBehaviour
 {
     public AudioMixer mixer;
     public AudioSource SFX;
+
+    public GameObject menu;
+    public GameObject cutsceneGroup;
     
-    public void StartGame(string sceneName)
+/*    public void SwapScenes(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        //SceneManager.LoadScene(sceneName);
+        menuActivator();
+    }*/
+
+
+    //Turns off main menu graphics
+    public void menuActivator()
+    {
+        menu.SetActive(false);
+        cutsceneGroup.SetActive(true);
     }
 
     public void VolumeChange(float volume)
